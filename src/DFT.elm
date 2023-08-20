e2f02b799a50df3c23e01efea52e83b76b3bdbeb
James Leslie <jamesleslie@posteo.net>
2023-08-20 10:38:04 +0100

    Add comments to DFT

--- src/DFT.elm
+++ src/DFT.elm
@@ -3,7 +3,9 @@ module DFT exposing (dft, dftFloat, dftInt)
 import Array exposing (Array)
 import Complex exposing (Complex, add, multiply, polar, real, zero)
 
-
+{-| O(n^2) in the length of the array. Compute the Discrete Fourier Transform of the given
+array.
+ -}
 dft : Array Complex -> Array Complex
 dft array =
     let
@@ -15,9 +17,11 @@ dft array =
 
         foldFunction n k acc =
             let
+                -- The kth term of array
                 hk =
                     Maybe.withDefault zero <| Array.get k array
 
+                -- exp (2 pi i k n / N)
                 expTerm =
                     polar 1 <| 2 * pi * (toFloat <| k * n) / toFloat size
             in
@@ -26,14 +30,20 @@ dft array =
         fourierFunction n =
             Array.foldl (foldFunction n) zero indexArray
     in
-    Array.map fourierFunction indexArray
+    Array.initialize size fourierFunction
 
 
+{-| O(n^2) in the length of the array. Compute the Discrete Fourier Transform of the given
+integer array.
+ -}
 dftInt : Array Int -> Array Complex
 dftInt =
     dft << Array.map (real << toFloat)
 
 
+{-| O(n^2) in the length of the array. Compute the Discrete Fourier Transform of the given
+float array.
+ -}
 dftFloat : Array Float -> Array Complex
 dftFloat =
     dft << Array.map real
